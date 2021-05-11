66c98671c7755f4d778045a4c3c463a3b45e430a
SebLng <sebastian.lungu30@gmail.com>
2021-05-11 20:49:22 +0100

    variables++

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Olooooooooo Mes Establos');
+let firstName = 'Feli';
+console.log(firstName);
+let lastName = 'CelViteaz';
+console.log(lastName);
+// cannot be reserved keyword
+// meaningful
+// no space or hyphen (-)
+// case sensitive
+const interestRate = 0.3;
+console.log(interestRate);
