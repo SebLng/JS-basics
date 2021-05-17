1dc4f5419ab4fbd119e29baa903eb6340a5594a9
SebLng <sebastian.lungu30@gmail.com>
2021-05-17 19:52:50 +0100

    Strings & Numbers

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-console.log('Olooooooooo Mes Establos');
-let firstName = 'Feli';
-console.log(firstName);
-let lastName = 'CelViteaz';
-console.log(lastName);
+//printing
+
+document.write("<h1>Helios cel Felios</h1>");
+document.write("<hr>");
+document.write("<h2>ACT 1</h2>");
+document.write("<p>A fost odata ca-n povesti</p>"); 
+document.write("<p>A fost ca niciodata</p>");
+document.write("<p>Din rude mari imparatesti</p>");
+document.write('<p style="color: red">Un Felios ciubota (la JavaScript)</p>');
+console.log("Log1");
+
+//variables
+//example myFirstSlumberParty
+
+var name1 = "Susan";
+var name2 = "Megan";
+var name3 = "Lucy";
+
+var age = 18;
+
+var xyz;
+xyz = true;
+
+name1 = "Stacey";
+
+document.write("<h2> Act 2  It's Party Time with " + name1 + ", " + name2 + " and " + name3 +"</h2>")
+
+//Casting and Converting
+document.write(100 + Number("25") + "<br>");
+document.write(100 + parseFloat("50.99") + "<br>");
+
+//Strings
+
+var act = "Act 3 To be continued...";
+//indexes: 0123456789
+
+document.write("<h2>" +act + " is " + act.length + " characters long </h2>" + "<br>");
+document.write(act.charAt(0) + "<br>");
+document.write(act.indexOf("c") + "<br>");
+document.write(act.substring(9) + "<br>");
+document.write(act.substring(9, 11) + "<br>");
+
+// Numbers 
+
+
+
+
+//console.log('Olooooooooo Mes Establos');
+//let firstName = 'Feli';
+//console.log(firstName);
+//let lastName = 'CelViteaz';
+//console.log(lastName);
 // cannot be reserved keyword
 // meaningful
 // no space or hyphen (-)
 // case sensitive
-const interestRate = 0.3;
-console.log(interestRate);
+//const interestRate = 0.3;
+//console.log(interestRate);
+//alert("File JS");
